06ac6976e32aef91862626bd58144b3a17ebc79d
Hae-Won Park <hwp0223@gmail.com>
2015-08-05 21:10:15 +0900

    Create Devisionstringified.rb

---
+++ Devisionstringified.rb
@@ -0,0 +1,11 @@
+def DivisionStringified(num1,num2)
+
+  # code goes here
+  answer=num1/num2.round
+  return answer
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+DivisionStringified(STDIN.gets)           
