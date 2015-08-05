543066cc8f4085fff52abc4a6b6fe4096083cfc7
Hae-Won Park <hwp0223@gmail.com>
2015-08-05 21:05:12 +0900

    Create Check nums.rb

---
+++ Check nums.rb
@@ -0,0 +1,21 @@
+def CheckNums(num1,num2)
+
+  # code goes here
+  if num1<num2
+    num1="true"
+  elsif num1>num2
+    num1="false"
+  else
+    num1="-1"
+  end
+  
+  
+  
+  
+  return num1 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+CheckNums(STDIN.gets)           
